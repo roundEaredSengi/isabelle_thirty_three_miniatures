0994581e95a1371a945e8238fbebfc6e99770fc9
MisterPine <git@mr-pine.de>
2026-01-14 16:59:47 +0100

    Do some cleanup

--- Miniature_Three.thy
+++ Miniature_Three.thy
@@ -27,17 +27,57 @@ fun is_club_fam :: "'c list \<Rightarrow> 'c family \<Rightarrow> bool" where
 
 text \<open>Oddtown Theorem\<close>
 
-lemma hui:
+lemma mod2_0_1_id:
   fixes 
     x :: nat
   assumes 
     "x \<in> {0, 1}"
   shows
-    "Abs_gf2 x = Abs_gf2 (x mod 2)"
+    "x = x mod 2"
   using assms
   by fastforce
 
-lemma sum_mod_2:
+lemma abs_gf2_homom_mod2: "Abs_gf2 (x mod 2) + Abs_gf2 (y mod 2) = Abs_gf2 ((x + y) mod 2)"
+proof (cases "even x")
+  case x_even: True
+  then have x_zero: "Abs_gf2 (x mod 2) = 0" using zero_gf2_def by simp
+  then show ?thesis
+  proof (cases "even y")
+    case y_even: True
+    then have "Abs_gf2 (y mod 2) = 0" using zero_gf2_def by simp
+    moreover have "Abs_gf2 ((x+y) mod 2) = 0" using  x_even y_even zero_gf2_def by simp
+    ultimately show ?thesis using x_zero by algebra
+  next
+    case y_odd: False
+    then have "(y mod 2) = 1" by presburger
+    then have "Abs_gf2 (y mod 2) = 1"  using one_gf2_def by argo
+    moreover have "(x+y) mod 2 = 1" using x_even y_odd by presburger
+    then have "Abs_gf2 ((x+y) mod 2) = 1" using one_gf2_def by simp
+    ultimately show ?thesis using x_zero by algebra
+  qed  
+next
+  case x_odd: False
+  then have "x mod 2 = 1" by presburger
+  then have x_one: "Abs_gf2 (x mod 2) = 1" using one_gf2_def by simp
+  then show ?thesis
+  proof (cases "even y")
+    case y_even: True
+    then have "Abs_gf2 (y mod 2) = 0" using zero_gf2_def by simp
+    moreover have "(x+y) mod 2 = 1" using x_odd y_even by presburger
+    then have "Abs_gf2 ((x+y) mod 2) = 1" using one_gf2_def by simp
+    ultimately show ?thesis using x_one by algebra
+  next
+    case y_odd: False
+    then have "(y mod 2) = 1" by presburger
+    then have "Abs_gf2 (y mod 2) = 1"  using one_gf2_def by argo
+    moreover have "(x+y) mod 2 = 0" using x_odd y_odd by presburger
+    then have "Abs_gf2 ((x+y) mod 2) = 0" using zero_gf2_def by simp
+    moreover have "(1::gf2) + 1 = 0" unfolding plus_gf2_def zero_gf2_def by simp
+    ultimately show ?thesis using x_one by argo 
+  qed
+qed
+
+lemma sum_mod_2_gf2:
   fixes 
     f :: "'x \<Rightarrow> nat" and
     X :: "'x set"
@@ -68,31 +108,14 @@ next
     "Abs_gf2 (sum f (X-{a}) mod 2) = sum (Abs_gf2 \<circ> f) (X-{a})"
     using Suc.hyps Suc.prems
     by blast
-  hence f_mod_2:
-    "(Abs_gf2 \<circ> f) a = Abs_gf2 (f a mod 2)"
-    unfolding comp_def
-    using hui[of "f a"] assms
-    by blast
-  have "(sum f (X-{a}) mod 2) = (sum f X - f a) mod 2"
-    using \<open>a \<in> X\<close> Suc.prems sum_diff1_nat[of f X a]
-    by simp
-  hence "Abs_gf2 (sum f X mod 2) = Abs_gf2 ((sum f (X-{a}) + f a) mod 2)"
-    by (metis Suc.prems \<open>a \<in> X\<close> add.commute sum.remove)
-  also have "... = Abs_gf2 (((sum f (X-{a}) mod 2 + (f a) mod 2)) mod 2)"
-    by (metis mod_add_eq)
-  also have "... = Abs_gf2 ((sum f (X-{a})) mod 2) + Abs_gf2 (f a mod 2)"
-    by (metis (lifting) Suc_1 Suc_eq_plus1 add.comm_neutral add.commute bits_one_mod_two_eq_one
-        diff_numeral_special(9) minus_gf2_def mod2_Suc_Suc mod_self not_mod2_eq_Suc_0_eq_0 
-        one_gf2.abs_eq plus_gf2_def zero_gf2.abs_eq)
-  also have "... = sum (Abs_gf2 \<circ> f) (X-{a}) + Abs_gf2 (f a mod 2)"
-    using ind_hyp
-    by simp
-  also have "... = sum (Abs_gf2 \<circ> f) (X-{a}) + (Abs_gf2 \<circ> f) a"
-    using f_mod_2
-    by simp
-  finally show ?case
-    using \<open>a \<in> X\<close>
-    by (metis Suc.prems add.commute sum.remove)
+  have "(sum f X mod 2) = ((f a + sum f (X-{a})) mod 2)"
+    by (metis \<open>a \<in> X\<close> Suc.prems sum.remove)
+  then have "Abs_gf2 (sum f X mod 2) = Abs_gf2 (f a mod 2) + (Abs_gf2 (sum f (X-{a}) mod 2))" using abs_gf2_homom_mod2 by presburger
+  moreover  have "\<dots> = Abs_gf2 (f a mod 2) + (sum (Abs_gf2 \<circ> f) (X - {a}))" using ind_hyp by argo
+  moreover  have "\<dots> = Abs_gf2 (f a) + (sum (Abs_gf2 \<circ> f) (X - {a}))" using assms mod2_0_1_id by presburger
+  moreover  have "\<dots> = (Abs_gf2 \<circ> f) a + (sum (Abs_gf2 \<circ> f) (X - {a}))" by simp
+  moreover  have "\<dots> = sum (Abs_gf2 \<circ> f) X" using \<open>a \<in> X\<close> Suc.prems sum.remove by metis
+  ultimately show ?case by argo
 qed
 
 lemma set_card: 
@@ -188,43 +211,47 @@ proof -
   proof (safe)
     fix i :: nat and j :: nat
     assume "i < length \<F>" and "j < length \<F>"
+    let ?index_in_inter = "\<lambda>k. (X!k) \<in> (\<F>!i \<inter> \<F>!j)"
+    let ?in_inter_ind = "\<lambda>k. if ?index_in_inter(k) then 1 else 0"
+    let ?index_range = "{0 ..< length X}"
+    let ?inter_indices = "{k | k. k \<in> ?index_range \<and> ?index_in_inter(k)}"
     have valued_0_1:
-      "\<forall>k. (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0) k \<in> {0::nat, 1}"
+      "\<forall>k. ?in_inter_ind k \<in> {0::nat, 1}"
       by simp
     have
-      "sum (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0) {0 ..< length X} 
-        = card {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)}"
-      using set_card[of "{0 ..< length X}" "\<lambda>k. X!k \<in> \<F>!i \<inter> \<F>!j"]
+      "sum ?in_inter_ind ?index_range 
+        = card ?inter_indices"
+      using set_card[of ?index_range ?index_in_inter]
       by simp
     hence
-      "Abs_gf2 (sum (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0) {0 ..< length X} mod 2)
-        = Abs_gf2 (card {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)} mod 2)"
+      "Abs_gf2 (sum ?in_inter_ind ?index_range mod 2)
+        = Abs_gf2 (card ?inter_indices mod 2)"
       by simp
     moreover have
-      "Abs_gf2 (sum (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0) {0 ..< length X} mod 2)
-        = (sum (Abs_gf2 \<circ> (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0)) {0 ..< length X})"
-      using sum_mod_2[of "\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0" "{0 ..< length X}"] valued_0_1
+      "Abs_gf2 (sum ?in_inter_ind ?index_range mod 2)
+        = (sum (Abs_gf2 \<circ> ?in_inter_ind) ?index_range)"
+      using sum_mod_2_gf2[of ?in_inter_ind ?index_range] valued_0_1
       by simp
     moreover have 
-      "(Abs_gf2 \<circ> (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0))
-        = (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then Abs_gf2 1 else Abs_gf2 0)"
+      "(Abs_gf2 \<circ> ?in_inter_ind)
+        = (\<lambda>k. if ?index_in_inter(k) then Abs_gf2 1 else Abs_gf2 0)"
       by auto
     ultimately have
-      "(sum (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then Abs_gf2 1 else Abs_gf2 0) {0 ..< length X})
-        = Abs_gf2 (card {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)} mod 2)"
+      "(sum (\<lambda>k. if ?index_in_inter(k) then Abs_gf2 1 else Abs_gf2 0) ?index_range)
+        = Abs_gf2 (card ?inter_indices mod 2)"
       by metis
     hence sum:
-      "(sum (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1::gf2 else 0::gf2) {0 ..< length X})
-        = Abs_gf2 (card {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)} mod 2)"
+      "(sum (\<lambda>k. if ?index_in_inter(k) then 1::gf2 else 0::gf2) ?index_range)
+        = Abs_gf2 (card ?inter_indices mod 2)"
       by (metis one_gf2_def zero_gf2_def)
-    have "inj_on (\<lambda>k. X!k) {0 ..< length X}"
+    have "inj_on (\<lambda>k. X!k) ?index_range"
       unfolding inj_on_def
       using village atLeast0LessThan 
       by auto
-    hence "inj_on (\<lambda>k. X!k) {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)}"
+    hence "inj_on (\<lambda>k. X!k) ?inter_indices"
       by (simp add: inj_on_def)
     moreover have 
-      "(\<lambda>k. X!k) ` {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)} = \<F>!i \<inter> \<F>!j"
+      "(\<lambda>k. X!k) ` ?inter_indices = \<F>!i \<inter> \<F>!j"
     proof (safe)
       fix
         x :: 'c
@@ -234,32 +261,32 @@ proof -
       hence "x \<in> set X" 
         using valid \<open>i < length \<F>\<close> \<open>j < length \<F>\<close>
         by auto
-      with this obtain k :: nat where "k \<in> {0 ..< length X}" and "x = X!k"
+      with this obtain k :: nat where "k \<in> ?index_range" and "x = X!k"
         by (metis imageE list.set_map map_nth set_upt)
-      thus "x \<in> (!) X ` {k |k. k \<in> {0..<length X} \<and> X ! k \<in> \<F> ! i \<inter> \<F> ! j}"
+      thus "x \<in> (!) X ` ?inter_indices"
         using \<open>x \<in> \<F>!i\<close> \<open>x \<in> \<F>!j\<close>
         by simp
     qed
     ultimately have
-      "bij_betw (\<lambda>k. X!k) {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)} (\<F>!i \<inter> \<F>!j)"
+      "bij_betw (\<lambda>k. X!k) ?inter_indices (\<F>!i \<inter> \<F>!j)"
       unfolding bij_betw_def
       by simp
     hence card:
-      "card {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)} = card (\<F>!i \<inter> \<F>!j)"
+      "card ?inter_indices = card (\<F>!i \<inter> \<F>!j)"
       by (rule bij_betw_same_card)
     have 
-      "\<forall>k \<in> {0 ..< length X}. (row ?A i) $ k = (if (X!k) \<in> (\<F>!i) then 1 else 0)"
+      "\<forall>k \<in> ?index_range. (row ?A i) $ k = (if (X!k) \<in> (\<F>!i) then 1 else 0)"
       unfolding inc_mat_of_def
       by (simp add: \<open>i < length \<F>\<close>)
     moreover have
-      "\<forall>k \<in> {0 ..< length X}. (col ?A\<^sup>T j) $ k = (row ?A j) $ k"
+      "\<forall>k \<in> ?index_range. (col ?A\<^sup>T j) $ k = (row ?A j) $ k"
       by (simp add: \<open>j < length \<F>\<close> inc_mat_dim_col)
     ultimately have 
-      "\<forall>k \<in> {0 ..< length X}. (row ?A i) $ k * (col ?A\<^sup>T j) $ k 
+      "\<forall>k \<in> ?index_range. (row ?A i) $ k * (col ?A\<^sup>T j) $ k 
         = (if (X!k) \<in> (\<F>!i) then 1 else 0) * (if (X!k) \<in> (\<F>!j) then 1 else 0)"
       by (simp add: \<open>j < length \<F>\<close> inc_mat_col_def)
     hence intersect:
-      "\<forall>k \<in> {0 ..< length X}. (row ?A i) $ k * (col ?A\<^sup>T j) $ k 
+      "\<forall>k \<in> ?index_range. (row ?A i) $ k * (col ?A\<^sup>T j) $ k 
         = (if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0)"
       by simp
     have "(?A * ?A\<^sup>T) $$ (i, j) = row ?A i \<bullet> col ?A\<^sup>T j"
@@ -269,13 +296,13 @@ proof -
       "row ?A i \<bullet> col ?A\<^sup>T j = sum (\<lambda>k. (row ?A i) $ k * (col ?A\<^sup>T j) $ k) {0 ..< dim_vec (row ?A i)}"
       unfolding scalar_prod_def
       by simp
-    also have "... = sum (\<lambda>k. (row ?A i) $ k * (col ?A\<^sup>T j) $ k) {0 ..< length X}"
+    also have "... = sum (\<lambda>k. (row ?A i) $ k * (col ?A\<^sup>T j) $ k) ?index_range"
       using \<open>i < length \<F>\<close> dim_A
       by simp
-    also have "... = sum (\<lambda>k. if (X!k) \<in> (\<F>!i \<inter> \<F>!j) then 1 else 0) {0 ..< length X}"
+    also have "... = sum ?in_inter_ind ?index_range"
       using intersect
       by simp
-    also have "... = Abs_gf2 (card {k | k. k \<in> {0 ..< length X} \<and> (X!k) \<in> (\<F>!i \<inter> \<F>!j)} mod 2)"
+    also have "... = Abs_gf2 (card ?inter_indices mod 2)"
       using sum
       by simp
     also have "... = Abs_gf2 (card (\<F>!i \<inter> \<F>!j) mod 2)"
